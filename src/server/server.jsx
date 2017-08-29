db689aba7deb66d09b6a562ac3bd2274133e36a7
mathuramg <mmg9291@gmail.com>
2017-08-29 18:53:21 -0400

    ensure the zububu and wiggy cannot be directly found

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -8,10 +8,13 @@ app.listen(process.env.PORT ||8080);
 console.log('running at port 8080');
 app.use('/', express.static('public/')); // set a static file directory
 
-function loggedIn(req, res, next) {
-    next();
-}
 
-app.get('*', loggedIn, function (req, res) {
+app.get('/lesson2_zububu', function (req, res) {
+  res.redirect('/lesson2_simonsays');
+});
+app.get('/lesson1_wiggy', function (req, res) {
+  res.redirect('/lesson1_beatmachine');
+});
+app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, '../../public/index.html'))
 });
